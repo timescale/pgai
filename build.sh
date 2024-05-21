c2ef5d091c4c7190a94131856f84635cd4d13b38
John Pruitt <jgpruitt@gmail.com>
2024-05-21 10:06:01 -0500

    added license

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-sudo cp /pgai/src/* `pg_config --sharedir`/extension
-psql --echo-errors --echo-queries -v VERBOSITY=verbose -f - <<EOF
-drop extension if exists ai cascade;
-create extension ai cascade;
-EOF
