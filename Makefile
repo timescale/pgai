b3b8dd2545f10d8371a8ba197ea6399b51655536
John Pruitt <jgpruitt@gmail.com>
2024-05-24 22:27:53 -0500

    Better virtual machine

--- Makefile
+++ Makefile
@@ -11,11 +11,8 @@ PG_SHAREDIR := $(shell pg_config --sharedir)
 EXTENSION_DIR := $(PG_SHAREDIR)/extension
 
 # Files to be installed
-SQL_FILES := $(wildcard *.sql)
-CONTROL_FILE := $(wildcard *.control)
-
-# Python packages required
-PYTHON_PACKAGES := openai tiktoken
+SQL_FILES := $(wildcard $(PGAI_EXTENSION)--*.sql)
+CONTROL_FILE := $(PGAI_EXTENSION).control
 
 # pgvector repository URL and default tag
 PGVECTOR_REPO := https://github.com/pgvector/pgvector.git
@@ -27,7 +24,6 @@ install: install_extension install_python_packages install_pgvector
 # Install the extension files
 install_extension:
 	@echo "Installing $(PGAI_EXTENSION) extension files to $(EXTENSION_DIR)"
-	@mkdir -p $(EXTENSION_DIR)
 	@cp $(SQL_FILES) $(EXTENSION_DIR)
 	@cp $(CONTROL_FILE) $(EXTENSION_DIR)
 	@echo "$(PGAI_EXTENSION) extension files installation complete."
@@ -35,7 +31,7 @@ install_extension:
 # Install the required Python packages
 install_python_packages:
 	@echo "Installing required Python packages: $(PYTHON_PACKAGES)"
-	@pip3 install --break-system-packages $(PYTHON_PACKAGES)
+	@pip3 install --break-system-packages -r requirements.txt
 	@echo "Python packages installation complete."
 
 # Install the pgvector extension
--- vm.sh
+++ vm.sh
@@ -2,34 +2,33 @@
 # creates a virtual machine using multipass for development use
 set -e
 multipass launch --name pgai lts
-multipass exec pgai -- bash <<EOF
-set -e
-echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
-sudo apt-get install -y gnupg postgresql-common apt-transport-https lsb-release wget
-sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh -y
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y \
-    postgresql-16 \
-    postgresql-plpython3-16 \
-    postgresql-16-pgvector \
-    python3-full \
-    python3-openai \
-    python3-tiktoken
-
-# dev requirements
-sudo apt-get install -y python3-pip
-echo 'PATH="$PATH:~/.local/bin"' >> ~/.profile
-pip install --break-system-packages pgspot
-
-sudo chmod go+w /usr/share/postgresql/16/extension/
-sudo chmod go+w /usr/lib/postgresql/16/lib/
-
-echo "/usr/bin/psql -U postgres -c \"create user ubuntu superuser login password 'ubuntu'\"" | sudo su postgres -
-echo "/usr/bin/psql -U postgres -c \"create database ubuntu owner ubuntu\"" | sudo su postgres -
-EOF
 multipass stop pgai
 multipass mount -t native . pgai:/pgai
 multipass start pgai
-multipass exec pgai -d /pgai -- cp ai--*.sql /usr/share/postgresql/16/extension/
-multipass exec pgai -d /pgai -- cp ai.control /usr/share/postgresql/16/extension/
+multipass exec pgai -- bash <<EOF
+set -eux
+sudo su root -
+
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+apt-get update
+apt-get upgrade -y
+apt-get install -y --no-install-recommends \
+    postgresql-16 \
+    postgresql-plpython3-16 \
+    postgresql-16-pgvector \
+    python3-pip
+
+pip install --break-system-packages -r /pgai/requirements.txt
+pip install --break-system-packages pgspot
+
+chmod go+w /usr/share/postgresql/16/extension/
+chmod go+w /usr/lib/postgresql/16/lib/
+
+echo "/usr/bin/psql -U postgres -c \"create user ubuntu superuser login password 'ubuntu'\"" | sudo su postgres -
+echo "/usr/bin/psql -U postgres -c \"create database ubuntu owner ubuntu\"" | sudo su postgres -
+
+cp /pgai/ai--*.sql /usr/share/postgresql/16/extension/
+cp /pgai/ai.control /usr/share/postgresql/16/extension/
+EOF
+multipass restart pgai
 multipass shell pgai
