23d33ca70579231d63f7a7d55c6067a5f92fb352
John Pruitt <jgpruitt@gmail.com>
2024-07-25 14:11:12 -0500

    Add linter and format checks

--- src/ai/ollama.py
+++ src/ai/ollama.py
@@ -3,7 +3,9 @@ from ollama import Client
 
 
 def get_ollama_host(plpy) -> str:
-    r = plpy.execute("select pg_catalog.current_setting('ai.ollama_host', true) as ollama_host")
+    r = plpy.execute(
+        "select pg_catalog.current_setting('ai.ollama_host', true) as ollama_host"
+    )
     if len(r) >= 0:
         return r[0]["ollama_host"]
     else:
