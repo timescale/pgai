33142d00e643232dfad8e8ed8db43ee262eaaf71
John Pruitt <jgpruitt@gmail.com>
2024-07-26 15:04:43 -0500

    Fixed silly logic bug

--- src/ai/openai.py
+++ src/ai/openai.py
@@ -7,10 +7,9 @@ def get_openai_api_key(plpy) -> str:
     r = plpy.execute(
         "select pg_catalog.current_setting('ai.openai_api_key', true) as api_key"
     )
-    if len(r) >= 0:
-        return r[0]["api_key"]
-    else:
+    if len(r) == 0:
         plpy.error("missing api key")
+    return r[0]["api_key"]
 
 
 def make_client(plpy, api_key: Optional[str] = None) -> openai.Client:
