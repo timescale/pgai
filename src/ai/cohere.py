94b1b8222c107363a3c92aa40997231fd0cf6160
John Pruitt <jgpruitt@gmail.com>
2024-07-26 15:04:43 -0500

    Fixed silly logic bug

--- src/ai/cohere.py
+++ src/ai/cohere.py
@@ -6,10 +6,9 @@ def find_api_key(plpy) -> str:
     r = plpy.execute(
         "select pg_catalog.current_setting('ai.cohere_api_key', true) as api_key"
     )
-    if len(r) >= 0:
-        return r[0]["api_key"]
-    else:
+    if len(r) == 0:
         plpy.error("missing api key")
+    return r[0]["api_key"]
 
 
 def make_client(plpy, api_key: Optional[str]) -> Client:
