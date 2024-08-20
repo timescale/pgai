8735596cb4464075cfcff7bc7bc4946642cae5e0
John Pruitt <jgpruitt@gmail.com>
2024-08-20 11:06:15 -0500

    Fix issue with background jobs

--- tests/server.py
+++ tests/server.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -5,7 +7,22 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class Vectorizer(BaseModel):
+    id: int
+    asynchronous: bool
+    external: bool
+    source_schema: str
+    source_table: str
+    source_pk: dict
+    target_schema: str
+    target_table: str
+    target_column: str
+    queue_schema: Optional[str] = None
+    queue_table: Optional[str] = None
+    config: dict
+
+
 @app.post("/")
 async def execute_vectorizer(vectorizer: dict):
-    return {"id": vectorizer['id']}
+    return {"id": vectorizer["id"]}
 
