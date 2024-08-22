f9a18501e1836d0de1ea1780a286ba98876ba357
John Pruitt <jgpruitt@gmail.com>
2024-08-22 12:02:30 -0500

    Allow list of roles to be granted privs

--- src/ai/vectorizer.py
+++ src/ai/vectorizer.py
@@ -29,6 +29,8 @@ def execute_async_ext_vectorizer(plpy, vectorizer_id: int) -> None:
     def post_vectorizer_execution(v: dict) -> httpx.Response:
         return httpx.post("http://localhost:8000/", json=v)
 
+    # TODO: what is the final URL for this? use a GUC and default
+
     r = post_vectorizer_execution(vectorizer)
     if r.status_code != httpx.codes.OK:
         plpy.error(f"failed to signal vectorizer execution: {r.status_code}")
