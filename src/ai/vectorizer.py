ea2cabd48ea1822521c9817aa0c35df150ee2f7e
John Pruitt <jgpruitt@gmail.com>
2024-08-28 07:58:48 -0500

    Collection of minor fixes

--- src/ai/vectorizer.py
+++ src/ai/vectorizer.py
@@ -12,7 +12,7 @@ def execute_async_ext_vectorizer(plpy, vectorizer_id: int) -> None:
         """
         select pg_catalog.to_jsonb(v) as vectorizer
         from ai.vectorizer v
-        where v.id = $1
+        where v.id operator(pg_catalog.=) $1
     """,
         ["int"],
     )
@@ -21,6 +21,7 @@ def execute_async_ext_vectorizer(plpy, vectorizer_id: int) -> None:
         plpy.error(f"vectorizer {vectorizer_id} not found")
     vectorizer = json.loads(result[0]["vectorizer"])
 
+    # get the url from a postgres setting if it has been set, otherwise use default
     plan = plpy.prepare("select pg_catalog.current_setting($1, true) as execute_vectorizer_url", ["text"])
     result = plan.execute([GUC_EXECUTE_VECTORIZER_URL], 1)
     url: str | None = None
@@ -42,4 +43,5 @@ def execute_async_ext_vectorizer(plpy, vectorizer_id: int) -> None:
 
     r = post_vectorizer_execution(vectorizer)
     if r.status_code != httpx.codes.OK:
+        # TODO: only display error text in debug log?
         plpy.error(f"failed to signal vectorizer execution: {r.status_code}\n{r.text}")
