d048f1ef5dc2e2f9e49b25d63cb469d7d781f16d
John Pruitt <jgpruitt@gmail.com>
2024-08-29 15:56:38 -0500

    Ensure that dump/restore works for non-superusers

--- tests/dump_restore/init.sql
+++ tests/dump_restore/init.sql
@@ -1,4 +1,3 @@
-create extension if not exists timescaledb;
 create extension if not exists ai cascade;
 
 create table blog
@@ -21,15 +20,12 @@ select ai.create_vectorizer
 ( 'blog'::regclass
 , embedding=>ai.embedding_openai('text-embedding-3-small', 768)
 , chunking=>ai.chunking_character_text_splitter('content', 128, 10)
-, formatting=>ai.formatting_python_template
+, formatting=>
+        ai.formatting_python_template
         ( 'title: $title published: $published $chunk'
         , columns=>array['title', 'published']
         )
-, scheduling=>ai.scheduling_timescaledb
-        ( interval '5m'
-        , initial_start=>'2050-01-06'::timestamptz
-        , timezone=>'America/Chicago'
-        )
+, scheduling=>ai.scheduling_none()
 , grant_to=>null
 );
 
