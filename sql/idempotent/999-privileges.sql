6a56c5b95d01ef8bb3d172dc4343ae23ce6f61b9
John Pruitt <jgpruitt@gmail.com>
2024-09-03 07:59:40 -0500

    Use session_user instead of pg_database_owner

--- sql/idempotent/999-privileges.sql
+++ sql/idempotent/999-privileges.sql
@@ -1,8 +1,8 @@
 
 
-grant all privileges on schema ai to pg_database_owner with grant option;
-grant all privileges on all tables in schema ai to pg_database_owner with grant option;
-grant all privileges on all sequences in schema ai to pg_database_owner with grant option;
-grant all privileges on all procedures in schema ai to pg_database_owner with grant option;
-grant all privileges on all functions in schema ai to pg_database_owner with grant option;
+grant all privileges on schema ai to session_user with grant option;
+grant all privileges on all tables in schema ai to session_user with grant option;
+grant all privileges on all sequences in schema ai to session_user with grant option;
+grant all privileges on all procedures in schema ai to session_user with grant option;
+grant all privileges on all functions in schema ai to session_user with grant option;
 
