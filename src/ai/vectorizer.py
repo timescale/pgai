a727247fe02b3bf1fef23f83b5662c8120d436ed
John Pruitt <jgpruitt@gmail.com>
2024-09-04 07:58:04 -0500

    Don't send http request on delete vectorizer

--- src/ai/vectorizer.py
+++ src/ai/vectorizer.py
@@ -4,11 +4,8 @@ import httpx
 import backoff
 from backoff._typing import Details
 
-GUC_EXECUTE_VECTORIZER_URL = "ai.execute_vectorizer_url"
-DEFAULT_EXECUTE_VECTORIZER_URL = "http://localhost:8000"
-
-GUC_DELETE_VECTORIZER_URL = "ai.execute_vectorizer_url"
-DEFAULT_DELETE_VECTORIZER_URL = "http://localhost:8000/vectorizer/delete"
+GUC_VECTORIZER_URL = "ai.vectorizer_url"
+DEFAULT_VECTORIZER_URL = "http://localhost:8000"
 
 
 def get_guc_value(plpy, setting: str, default: str) -> str:
@@ -36,7 +33,7 @@ def execute_async_ext_vectorizer(plpy, vectorizer_id: int) -> None:
         plpy.error(f"vectorizer {vectorizer_id} not found")
     vectorizer = json.loads(result[0]["vectorizer"])
 
-    the_url = get_guc_value(plpy, GUC_EXECUTE_VECTORIZER_URL, DEFAULT_EXECUTE_VECTORIZER_URL)
+    the_url = get_guc_value(plpy, GUC_VECTORIZER_URL, DEFAULT_VECTORIZER_URL)
 
     def on_backoff(detail: Details):
         plpy.warning(
@@ -51,25 +48,4 @@ def execute_async_ext_vectorizer(plpy, vectorizer_id: int) -> None:
 
     r = post()
     if r.status_code != httpx.codes.OK:
-        # TODO: only display error text in debug log?
-        plpy.error(f"failed to signal vectorizer execution: {r.status_code}\n{r.text}")
-
-
-def delete_vectorizer(plpy, vectorizer_id: int) -> None:
-    the_url = get_guc_value(plpy, GUC_DELETE_VECTORIZER_URL, DEFAULT_DELETE_VECTORIZER_URL)
-
-    def on_backoff(detail: Details):
-        plpy.warning(
-            f"{vectorizer_id} retry: {detail['tries']} elapsed: {detail['elapsed']} wait: {detail['wait']}..."
-        )
-
-    @backoff.on_exception(
-        backoff.expo, httpx.HTTPError, max_tries=10, max_time=120, on_backoff=on_backoff, raise_on_giveup=True
-    )
-    def get() -> httpx.Response:
-        return httpx.get(the_url + f"/{vectorizer_id}")
-
-    r = get()
-    if r.status_code != httpx.codes.OK:
-        # TODO: only display error text in debug log?
-        plpy.error(f"failed to signal vectorizer deletion: {r.status_code}\n{r.text}")
+        plpy.error(f"failed to signal vectorizer execution: {r.status_code}", detail=r.text)
