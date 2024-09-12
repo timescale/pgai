defec5b4277823fcb283f202448345b2cbf7c17f
John Pruitt <jgpruitt@gmail.com>
2024-09-12 10:45:20 -0500

    GUC rename

--- tests/vectorizer/test_vectorizer.py
+++ tests/vectorizer/test_vectorizer.py
@@ -339,8 +339,8 @@ def test_vectorizer_timescaledb():
             assert actual == 1
 
             # check that using the GUCs work
-            cur.execute("select set_config('cloud.external_function_executor_url', 'http://localhost:8000', false)")
-            cur.execute("select set_config('cloud.external_functions_executor_events_path', '/api/v1/events', false)")
+            cur.execute("select set_config('ai.external_function_executor_url', 'http://localhost:8000', false)")
+            cur.execute("select set_config('ai.external_functions_executor_events_path', '/api/v1/events', false)")
 
             # ping the external job explicitly
             # language=PostgreSQL
