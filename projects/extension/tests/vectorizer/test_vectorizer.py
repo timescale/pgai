49ced60d12019556a9f5be44fe48e87af50f1d93
John Pruitt <jgpruitt@gmail.com>
2024-10-10 09:05:16 -0500

    feat: detect naming collisions and raise helpful errors

--- projects/extension/tests/vectorizer/test_vectorizer.py
+++ projects/extension/tests/vectorizer/test_vectorizer.py
@@ -1004,3 +1004,117 @@ def test_index_create_concurrency():
             )
             actual = cur.fetchone()[0]
             assert actual == 1
+
+
+def test_naming_collisions():
+    with psycopg.connect(
+        db_url("test"), autocommit=True, row_factory=namedtuple_row
+    ) as con:
+        with con.cursor() as cur:
+            cur.execute("create extension if not exists ai cascade")
+            cur.execute("create extension if not exists timescaledb")
+            cur.execute("create schema if not exists vec")
+            cur.execute("drop table if exists vec.note4")
+            cur.execute("""
+                create table vec.note4
+                ( id bigint not null primary key generated always as identity
+                , note text not null
+                )
+            """)
+
+            # create a vectorizer for the table
+            # language=PostgreSQL
+            cur.execute("""
+            select ai.create_vectorizer
+            ( 'vec.note4'::regclass
+            , embedding=>ai.embedding_openai('text-embedding-3-small', 3)
+            , chunking=>ai.chunking_character_text_splitter('note')
+            , scheduling=>ai.scheduling_none()
+            , indexing=>ai.indexing_none()
+            , grant_to=>null
+            , enqueue_existing=>false
+            );
+            """)
+
+            # try to create another one, fail on target_table name collision
+            # language=PostgreSQL
+            with pytest.raises(
+                psycopg.errors.RaiseException,
+                match=".*specify an alternate target_table explicitly*",
+            ):
+                cur.execute("""
+                select ai.create_vectorizer
+                ( 'vec.note4'::regclass
+                , embedding=>ai.embedding_openai('text-embedding-3-small', 3)
+                , chunking=>ai.chunking_character_text_splitter('note')
+                , scheduling=>ai.scheduling_none()
+                , indexing=>ai.indexing_none()
+                , grant_to=>null
+                , enqueue_existing=>false
+                );
+                """)
+
+            # try to create another one, fail on queue_table name collision
+            # language=PostgreSQL
+            with pytest.raises(
+                psycopg.errors.RaiseException,
+                match=".*specify an alternate queue_table explicitly*",
+            ):
+                cur.execute("""
+                select ai.create_vectorizer
+                ( 'vec.note4'::regclass
+                , embedding=>ai.embedding_openai('text-embedding-3-small', 3)
+                , chunking=>ai.chunking_character_text_splitter('note')
+                , scheduling=>ai.scheduling_none()
+                , indexing=>ai.indexing_none()
+                , grant_to=>null
+                , enqueue_existing=>false
+                , target_schema=>'vec'
+                , target_table=>'note4_embedding_store2'
+                , queue_schema=>'ai'
+                , queue_table=>'_vectorizer_q_1'
+                );
+                """)
+
+            # try to create another one, fail on view name collision
+            # language=PostgreSQL
+            with pytest.raises(
+                psycopg.errors.RaiseException,
+                match=".*specify an alternate view_name explicitly*",
+            ):
+                cur.execute("""
+                select ai.create_vectorizer
+                ( 'vec.note4'::regclass
+                , embedding=>ai.embedding_openai('text-embedding-3-small', 3)
+                , chunking=>ai.chunking_character_text_splitter('note')
+                , scheduling=>ai.scheduling_none()
+                , indexing=>ai.indexing_none()
+                , grant_to=>null
+                , enqueue_existing=>false
+                , target_schema=>'vec'
+                , target_table=>'note4_embedding_store2'
+                , queue_schema=>'ai'
+                , queue_table=>'this_is_a_queue_table'
+                );
+                """)
+
+            # try to create another one, this should work
+            # language=PostgreSQL
+            cur.execute("""
+            select ai.create_vectorizer
+            ( 'vec.note4'::regclass
+            , embedding=>ai.embedding_openai('text-embedding-3-small', 3)
+            , chunking=>ai.chunking_character_text_splitter('note')
+            , scheduling=>ai.scheduling_none()
+            , indexing=>ai.indexing_none()
+            , grant_to=>null
+            , enqueue_existing=>false
+            , target_schema=>'vec'
+            , target_table=>'note4_embedding_store2'
+            , queue_schema=>'ai'
+            , queue_table=>'this_is_a_queue_table'
+            , view_schema=>'vec'
+            , view_name=>'note4_embedding2'
+            );
+            """)
+            assert True
