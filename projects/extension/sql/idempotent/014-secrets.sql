ac189368437eb3d2c9f44d7f7a42f0c6ab6c97ec
John Pruitt <jgpruitt@gmail.com>
2024-11-14 12:57:31 -0600

    chore: formatting

--- projects/extension/sql/idempotent/014-secrets.sql
+++ projects/extension/sql/idempotent/014-secrets.sql
@@ -13,21 +13,26 @@ $python$
 language plpython3u stable security invoker
 set search_path to pg_catalog, pg_temp;
 
-
+-------------------------------------------------------------------------------
+-- secret_permissions
 create or replace view ai.secret_permissions as
-SELECT * 
-FROM ai._secret_permissions
-WHERE pg_catalog.to_regrole("role") is not null 
-      AND pg_catalog.pg_has_role(current_user, "role", 'member');
+select *
+from ai._secret_permissions
+where pg_catalog.to_regrole("role") is not null
+      and pg_catalog.pg_has_role(current_user, "role", 'member');
 
+-------------------------------------------------------------------------------
+-- grant_secret
 create or replace function ai.grant_secret(secret_name text, grant_to_role text) returns void
 as $func$
-    insert into ai._secret_permissions (name, "role") VALUES (secret_name, grant_to_role);
+    insert into ai._secret_permissions ("name", "role") values (secret_name, grant_to_role);
 $func$ language sql volatile security invoker
 set search_path to pg_catalog, pg_temp;
 
+-------------------------------------------------------------------------------
+-- revoke_secret
 create or replace function ai.revoke_secret(secret_name text, revoke_from_role text) returns void
 as $func$
-    delete from ai._secret_permissions where name = secret_name and "role" = revoke_from_role;
+    delete from ai._secret_permissions where "name" = secret_name and "role" = revoke_from_role;
 $func$ language sql volatile security invoker
 set search_path to pg_catalog, pg_temp;
