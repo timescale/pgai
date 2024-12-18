3149904db2a514cd1aee4dae53c60ea2e9a234cf
Sergio Moya <1083296+smoya@users.noreply.github.com>
2024-12-18 13:17:40 +0100

    test: do not start the ollama container if OLLAMA_HOST is set

--- projects/pgai/tests/vectorizer/test_vectorizer_cli.py
+++ projects/pgai/tests/vectorizer/test_vectorizer_cli.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import time
 from collections.abc import Generator
+from pathlib import Path
 from typing import Any
 
 import openai
@@ -11,8 +12,8 @@ import pytest
 from click.testing import CliRunner
 from psycopg import Connection, sql
 from psycopg.rows import dict_row
-from testcontainers.postgres import PostgresContainer  # type: ignore
 from testcontainers.ollama import OllamaContainer  # type: ignore
+from testcontainers.postgres import PostgresContainer  # type: ignore
 
 from pgai.cli import vectorizer_worker
 from tests.vectorizer import expected
@@ -147,25 +148,31 @@ def configured_openai_vectorizer_id(
         return vectorizer_id
 
 
+@pytest.fixture(scope="session")
+def ollama_connection_url():
+    # If the OLLAMA_HOST environment variable is set, we assume that the user
+    # has an Ollama container running and we don't need to start a new one.
+    if "OLLAMA_HOST" in os.environ:
+        yield os.environ["OLLAMA_HOST"]
+    else:
+        with OllamaContainer(
+            image="ollama/ollama:latest", ollama_home=Path.home() / ".ollama"
+        ) as ollama:
+            yield ollama.get_endpoint()
+
+
 @pytest.fixture
 def configured_ollama_vectorizer_id(
     source_table: str,
     cli_db: tuple[TestDatabase, Connection],
     test_params: tuple[int, int, int, str, str],
-    ollama_container: OllamaContainer,
+    ollama_connection_url: str,
 ) -> int:
     """Creates and configures an ollama vectorizer for testing"""
     _, concurrency, batch_size, chunking, formatting = test_params
     _, conn = cli_db
 
-    base_url = ollama_container.get_endpoint()
-    if "OLLAMA_HOST" in os.environ: 
-        # The user is running it's own Ollama container
-        base_url = os.environ["OLLAMA_HOST"]
-        ollama_container.stop()
-
     with conn.cursor(row_factory=dict_row) as cur:
-        
         # Create vectorizer
         cur.execute(f"""
             SELECT ai.create_vectorizer(
@@ -173,7 +180,7 @@ def configured_ollama_vectorizer_id(
                 embedding => ai.embedding_ollama(
                     'nomic-embed-text',
                     768,
-                    base_url => '{base_url}'
+                    base_url => '{ollama_connection_url}'
                 ),
                 chunking => ai.{chunking},
                 formatting => ai.{formatting},
