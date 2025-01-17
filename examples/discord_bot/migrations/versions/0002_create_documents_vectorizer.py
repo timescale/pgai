857b64eededab2454b1078a2cc9deb934f1559ed
Jascha <jascha@timescale.com>
2025-01-17 14:34:02 +0100

    chore: formatting

--- examples/discord_bot/migrations/versions/0002_create_documents_vectorizer.py
+++ examples/discord_bot/migrations/versions/0002_create_documents_vectorizer.py
@@ -5,17 +5,21 @@ Revises: 0001
 Create Date: 2025-01-07 17:18:58.091881
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
-from pgai.vectorizer.configuration import EmbeddingOpenaiConfig, ChunkingRecursiveCharacterTextSplitterConfig, \
-    FormattingPythonTemplateConfig
+from pgai.vectorizer.configuration import (
+    EmbeddingOpenaiConfig,
+    ChunkingRecursiveCharacterTextSplitterConfig,
+    FormattingPythonTemplateConfig,
+)
 
 # revision identifiers, used by Alembic.
-revision: str = '0002'
-down_revision: Union[str, None] = '0001'
+revision: str = "0002"
+down_revision: Union[str, None] = "0001"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -24,16 +28,13 @@ def upgrade() -> None:
     op.execute("CREATE EXTENSION IF NOT EXISTS ai CASCADE;")
     op.create_vectorizer(
         source_table="documents",
-        embedding=EmbeddingOpenaiConfig(
-            model='text-embedding-3-small',
-            dimensions=768
-        ),
+        embedding=EmbeddingOpenaiConfig(model="text-embedding-3-small", dimensions=768),
         chunking=ChunkingRecursiveCharacterTextSplitterConfig(
-            chunk_column='content',
+            chunk_column="content",
             chunk_size=800,
             chunk_overlap=200,
         ),
-        formatting=FormattingPythonTemplateConfig(template='$file_name \n $chunk')
+        formatting=FormattingPythonTemplateConfig(template="$file_name \n $chunk"),
     )
 
 
