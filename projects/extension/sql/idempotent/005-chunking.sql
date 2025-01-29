e8626abdea1fe44433c3a8100b37630cbbfcac11
Adol Rodriguez <adolsalamanca@gmail.com>
2025-01-29 11:55:56 +0100

    feat: tweak chunking func to support doc chunking
    chunking needs a special logic for document processing, given that we're
    no longer expecting a column name that should exist in the source table,
    but what's going to be chunked is the content of the file stored in the
    cloud object storage.
    
    Co-authored-by: Sergio Moya <1083296+smoya@users.noreply.github.com>

--- projects/extension/sql/idempotent/005-chunking.sql
+++ projects/extension/sql/idempotent/005-chunking.sql
@@ -2,11 +2,12 @@
 -------------------------------------------------------------------------------
 -- chunking_character_text_splitter
 create or replace function ai.chunking_character_text_splitter
-( chunk_column pg_catalog.name
+( chunk_column pg_catalog.name default ''
 , chunk_size pg_catalog.int4 default 800
 , chunk_overlap pg_catalog.int4 default 400
 , separator pg_catalog.text default E'\n\n'
 , is_separator_regex pg_catalog.bool default false
+, chunk_document pg_catalog.bool default false
 ) returns pg_catalog.jsonb
 as $func$
     select json_object
@@ -17,6 +18,7 @@ as $func$
     , 'chunk_overlap': chunk_overlap
     , 'separator': separator
     , 'is_separator_regex': is_separator_regex
+    , 'chunk_document': chunk_document
     absent on null
     )
 $func$ language sql immutable security invoker
@@ -26,11 +28,12 @@ set search_path to pg_catalog, pg_temp
 -------------------------------------------------------------------------------
 -- chunking_recursive_character_text_splitter
 create or replace function ai.chunking_recursive_character_text_splitter
-( chunk_column pg_catalog.name
+( chunk_column pg_catalog.name default ''
 , chunk_size pg_catalog.int4 default 800
 , chunk_overlap pg_catalog.int4 default 400
 , separators pg_catalog.text[] default array[E'\n\n', E'\n', '.', '?', '!', ' ', '']
 , is_separator_regex pg_catalog.bool default false
+, chunk_document pg_catalog.bool default false
 ) returns pg_catalog.jsonb
 as $func$
     select json_object
@@ -41,6 +44,7 @@ as $func$
     , 'chunk_overlap': chunk_overlap
     , 'separators': separators
     , 'is_separator_regex': is_separator_regex
+    , 'chunk_document': chunk_document
     absent on null
     )
 $func$ language sql immutable security invoker
@@ -60,6 +64,7 @@ declare
     _implementation pg_catalog.text;
     _chunk_column pg_catalog.text;
     _found pg_catalog.bool;
+    _chunk_document pg_catalog.bool;
 begin
     if pg_catalog.jsonb_typeof(config) operator(pg_catalog.!=) 'object' then
         raise exception 'chunking config is not a jsonb object';
@@ -75,21 +80,29 @@ begin
         raise exception 'invalid chunking config implementation';
     end if;
 
+    _chunk_document = config operator(pg_catalog.->>) 'chunk_document';
     _chunk_column = config operator(pg_catalog.->>) 'chunk_column';
+    if (_chunk_document is false and _chunk_column = '') or (_chunk_document is true and _chunk_column != '') then
+       raise exception 'either one of chunk_column or chunk_document should be set in config';
+    end if;
 
-    select count(*) operator(pg_catalog.>) 0 into strict _found
-    from pg_catalog.pg_class k
-    inner join pg_catalog.pg_namespace n on (k.relnamespace operator(pg_catalog.=) n.oid)
-    inner join pg_catalog.pg_attribute a on (k.oid operator(pg_catalog.=) a.attrelid)
-    inner join pg_catalog.pg_type y on (a.atttypid operator(pg_catalog.=) y.oid)
-    where n.nspname operator(pg_catalog.=) source_schema
-    and k.relname operator(pg_catalog.=) source_table
-    and a.attnum operator(pg_catalog.>) 0
-    and a.attname operator(pg_catalog.=) _chunk_column
-    and y.typname in ('text', 'varchar', 'char', 'bpchar')
-    ;
-    if not _found then
-        raise exception 'chunk column in config does not exist in the table: %', _chunk_column;
+    if _chunk_document is false then
+        _chunk_column = config operator(pg_catalog.->>) 'chunk_column';
+
+        select count(*) operator(pg_catalog.>) 0 into strict _found
+        from pg_catalog.pg_class k
+        inner join pg_catalog.pg_namespace n on (k.relnamespace operator(pg_catalog.=) n.oid)
+        inner join pg_catalog.pg_attribute a on (k.oid operator(pg_catalog.=) a.attrelid)
+        inner join pg_catalog.pg_type y on (a.atttypid operator(pg_catalog.=) y.oid)
+        where n.nspname operator(pg_catalog.=) source_schema
+        and k.relname operator(pg_catalog.=) source_table
+        and a.attnum operator(pg_catalog.>) 0
+        and a.attname operator(pg_catalog.=) _chunk_column
+        and y.typname in ('text', 'varchar', 'char', 'bpchar')
+        ;
+        if not _found then
+            raise exception 'chunk column in config does not exist in the table: %', _chunk_column;
+        end if;
     end if;
 end
 $func$ language plpgsql stable security invoker
