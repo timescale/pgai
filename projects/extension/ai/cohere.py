177957092bd7cf7003ff9baf4f87b7136aef3e51
John Pruitt <jgpruitt@gmail.com>
2025-02-03 15:21:41 -0600

    feat(cohere)!: update cohere embed, chat, rerank to use V2 endpoints

--- projects/extension/ai/cohere.py
+++ projects/extension/ai/cohere.py
@@ -1,7 +1,7 @@
-from cohere import Client
+from cohere import ClientV2
 
 DEFAULT_KEY_NAME = "COHERE_API_KEY"
 
 
-def make_client(api_key: str) -> Client:
-    return Client(api_key)
+def make_client(api_key: str) -> ClientV2:
+    return ClientV2(api_key=api_key)
