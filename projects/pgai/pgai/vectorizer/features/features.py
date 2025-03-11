3b8c2646a07f69eba8907752ec91000b8a2ab5c9
Adol Rodriguez <adolsalamanca@gmail.com>
2025-03-11 12:51:07 +0100

    chore: create retries feature

--- projects/pgai/pgai/vectorizer/features/features.py
+++ projects/pgai/pgai/vectorizer/features/features.py
@@ -17,3 +17,12 @@ class Features:
         table, and the `ai.vectorizer_status` view.
         """
         return self.ext_version > version.parse("0.7.0")
+
+    @cached_property
+    def loading_retries(self) -> bool:
+        """If the loading retries feature is supported by the extension.
+
+        The feature includes changes in the way we fetch_work from the
+        queueing tables, and also how we handle the retries.
+        """
+        return self.ext_version > version.parse("0.9.0")
