timescale/pgai

commit 3768c1c0a58e147fbeb6e720d7043f0fa6e0513a

feat: better worker tracking from the db (#499)
Records more information about the worker status in the database.
Allows more visibility from the db layer.

projects/pgai/pgai/cli.py
@@ -5,7 +5,6 @@ import os
 import random
 import signal
 import sys
-import time
 import traceback
 from collections.abc import Sequence
 from typing import Any
@@ -22,6 +21,7 @@ from .__init__ import __version__
 from .vectorizer.embeddings import ApiKeyMixin
 from .vectorizer.features import Features
 from .vectorizer.vectorizer import Vectorizer, Worker
+from .vectorizer.worker_tracking import WorkerTracking
 
 load_dotenv()
 
@@ -132,23 +132,33 @@ def get_vectorizer(db_url: str, vectorizer_id: int) -> Vectorizer:
         return vectorizer
 
 
-def run_vectorizer(
+async def run_vectorizer(
     db_url: str,
     vectorizer: Vectorizer,
     concurrency: int,
     features: Features,
+    worker_tracking: WorkerTracking,
 ) -> None:
-    async def run_workers(
-        db_url: str, vectorizer: Vectorizer, concurrency: int
-    ) -> list[int]:
-        tasks = [
-            asyncio.create_task(Worker(db_url, vectorizer, features).run())
-            for _ in range(concurrency)
-        ]
-        return await asyncio.gather(*tasks)
+    tasks = [
+        asyncio.create_task(Worker(db_url, vectorizer, features, worker_tracking).run())
+        for _ in range(concurrency)
+    ]
+    results = await asyncio.gather(*tasks, return_exceptions=True)
 
-    results = asyncio.run(run_workers(db_url, vectorizer, concurrency))
-    items = sum(results)
+    # raise any exceptions, but only after all tasks have completed
+    items: int = 0
+    exceptions: list[BaseException] = []
+    for result in results:
+        if isinstance(result, BaseException):
+            # report all exceptions
+            await worker_tracking.save_vectorizer_error(vectorizer.id, str(result))
+            exceptions.append(result)
+        else:
+            items += result
+
+    if len(exceptions) > 0:
+        raise Exception(exceptions)
+
     log.info("finished processing vectorizer", items=items, vectorizer_id=vectorizer.id)
 
 
@@ -256,6 +266,44 @@ def vectorizer_worker(
     once: bool,
     exit_on_error: bool | None,
 ) -> None:
+    asyncio.run(
+        async_run_vectorizer_worker(
+            db_url,
+            vectorizer_ids,
+            concurrency,
+            log_level,
+            poll_interval,
+            once,
+            exit_on_error,
+        )
+    )
+
+
+async def handle_error(
+    error_message: str,
+    vectorizer_id: int | None,
+    worker_tracking: WorkerTracking | None,
+    exit_on_error: bool | None,
+) -> None:
+    log.error(error_message)
+    if worker_tracking is not None:
+        await worker_tracking.save_vectorizer_error(vectorizer_id, error_message)
+    if exit_on_error:
+        if worker_tracking is not None:
+            await worker_tracking.force_last_heartbeat_and_stop()
+        log.info("exiting due to error")
+        sys.exit(1)
+
+
+async def async_run_vectorizer_worker(
+    db_url: str,
+    vectorizer_ids: Sequence[int],
+    concurrency: int,
+    log_level: str,
+    poll_interval: int,
+    once: bool,
+    exit_on_error: bool | None,
+) -> None:
     # gracefully handle being asked to shut down
     signal.signal(signal.SIGINT, shutdown_handler)
     signal.signal(signal.SIGTERM, shutdown_handler)
@@ -276,6 +324,8 @@ def vectorizer_worker(
         exit_on_error = True
 
     features = None
+    worker_tracking = None
+
     while True:
         try:
             if not can_connect or pgai_version is None:
@@ -284,15 +334,21 @@ def vectorizer_worker(
                     con.cursor(row_factory=namedtuple_row) as cur,
                 ):
                     pgai_version = get_pgai_version(cur)
-                    can_connect = True
                     if pgai_version is None:
-                        log.error("the pgai extension is not installed")
-                        if exit_on_error:
-                            sys.exit(1)
-                    else:
-                        features = Features(pgai_version)
+                        err_msg = "the pgai extension is not installed"
+                        await handle_error(
+                            err_msg, None, worker_tracking, exit_on_error
+                        )
+                        break
+                    features = Features(pgai_version)
 
-            if can_connect and pgai_version is not None and features is not None:
+                    worker_tracking = WorkerTracking(
+                        db_url, poll_interval, features, __version__
+                    )
+                    await worker_tracking.start()
+                    can_connect = True
+
+            if can_connect and features is not None and worker_tracking is not None:
                 if not dynamic_mode and len(valid_vectorizer_ids) != len(
                     vectorizer_ids
                 ):
@@ -301,11 +357,13 @@ def vectorizer_worker(
                         vectorizer_ids,
                     )
                     if len(valid_vectorizer_ids) != len(vectorizer_ids):
-                        log.error(
-                            f"invalid vectorizers, wanted: {list(vectorizer_ids)}, got: {valid_vectorizer_ids}"  # noqa: E501 (line too long)
+                        err_msg = f"invalid vectorizers, wanted: {list(vectorizer_ids)}, got: {valid_vectorizer_ids}"  # noqa: E501
+                        await handle_error(
+                            err_msg,
+                            None,
+                            worker_tracking,
+                            exit_on_error,
                         )
-                        if exit_on_error:
-                            sys.exit(1)
                 else:
                     valid_vectorizer_ids = get_vectorizer_ids(
                         db_url,
@@ -317,34 +375,40 @@ def vectorizer_worker(
                 for vectorizer_id in valid_vectorizer_ids:
                     try:
                         vectorizer = get_vectorizer(db_url, vectorizer_id)
-                        log.info("running vectorizer", vectorizer_id=vectorizer_id)
-                        run_vectorizer(db_url, vectorizer, concurrency, features)
                     except (VectorizerNotFoundError, ApiKeyNotFoundError) as e:
-                        log.error(
-                            f"error getting vectorizer: {type(e).__name__}: {str(e)} "
+                        err_msg = (
+                            f"error getting vectorizer: {type(e).__name__}: {str(e)}"
                         )
-                        if exit_on_error:
-                            sys.exit(1)
+                        await handle_error(
+                            err_msg, vectorizer_id, worker_tracking, exit_on_error
+                        )
+                        break
+
+                    log.info("running vectorizer", vectorizer_id=vectorizer_id)
+                    await run_vectorizer(
+                        db_url, vectorizer, concurrency, features, worker_tracking
+                    )
         except psycopg.OperationalError as e:
             if "connection failed" in str(e):
-                log.error(f"unable to connect to database: {str(e)}")
+                err_msg = f"unable to connect to database: {str(e)}"
             else:
-                log.error(f"unexpected error: {str(e)}")
-            if exit_on_error:
-                sys.exit(1)
+                err_msg = f"unexpected error: {str(e)}"
+            await handle_error(err_msg, None, worker_tracking, exit_on_error)
         except Exception as e:
             # catch any exceptions, log them, and keep on going
-            log.error(f"unexpected error: {str(e)}")
             for exception_line in traceback.format_exception(e):
                 for line in exception_line.rstrip().split("\n"):
                     log.debug(line)
-            if exit_on_error:
-                sys.exit(1)
-
+            err_msg = f"unexpected error: {str(e)}"
+            await handle_error(err_msg, None, worker_tracking, exit_on_error)
         if once:
+            if worker_tracking is not None:
+                await worker_tracking.force_last_heartbeat_and_stop()
+            log.info("once mode, exiting...")
             return
+
         log.info(f"sleeping for {poll_interval_str} before polling for new work")
-        time.sleep(poll_interval)
+        await asyncio.sleep(poll_interval)
 
 
 @click.group()

projects/extension/sql/idempotent/019-worker-tracking.sql
@@ -1,0 +1,48 @@
+CREATE OR REPLACE FUNCTION ai._worker_start(version text, expected_heartbeat_interval interval) RETURNS uuid AS $$
+DECLARE
+    worker_id uuid;
+BEGIN
+    --can add version check here
+    INSERT INTO ai.vectorizer_worker_process (version, expected_heartbeat_interval) VALUES (version, expected_heartbeat_interval) RETURNING id INTO worker_id;
+    RETURN worker_id;
+END;
+$$ LANGUAGE plpgsql security invoker
+set search_path to pg_catalog, pg_temp;
+
+CREATE OR REPLACE FUNCTION ai._worker_heartbeat(worker_id uuid, num_successes_since_last_heartbeat int, num_errors_since_last_heartbeat int, error_message text) RETURNS void AS $$
+DECLARE
+    heartbeat_timestamp timestamptz = clock_timestamp();
+BEGIN
+    UPDATE ai.vectorizer_worker_process SET 
+          last_heartbeat = heartbeat_timestamp 
+        , heartbeat_count = heartbeat_count + 1 
+        , error_count = error_count + num_errors_since_last_heartbeat
+        , success_count = success_count + num_successes_since_last_heartbeat
+        , last_error_message = CASE WHEN error_message IS NOT NULL THEN error_message ELSE last_error_message END 
+        , last_error_at = CASE WHEN error_message IS NOT NULL THEN heartbeat_timestamp ELSE last_error_at END 
+    WHERE id = worker_id;
+END;
+$$ LANGUAGE plpgsql security invoker
+set search_path to pg_catalog, pg_temp;
+
+CREATE OR REPLACE FUNCTION ai._worker_progress(worker_id uuid, worker_vectorizer_id int, num_successes int, error_message text) RETURNS void AS $$
+DECLARE
+    progress_timestamp timestamptz = clock_timestamp();
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM ai.vectorizer_worker_progress WHERE vectorizer_id = worker_vectorizer_id) THEN
+        --make sure a row exists for this vectorizer
+        INSERT INTO ai.vectorizer_worker_progress (vectorizer_id) VALUES (worker_vectorizer_id) ON CONFLICT DO NOTHING;
+    END IF;
+
+    UPDATE ai.vectorizer_worker_progress SET 
+        last_success_at = CASE WHEN error_message IS NULL THEN progress_timestamp ELSE last_success_at END
+      , last_success_process_id = CASE WHEN error_message IS NULL THEN worker_id ELSE last_success_process_id END
+      , last_error_at = CASE WHEN error_message IS NULL THEN last_error_at ELSE progress_timestamp END
+      , last_error_message = CASE WHEN error_message IS NULL THEN last_error_message ELSE error_message END
+      , last_error_process_id = CASE WHEN error_message IS NULL THEN last_error_process_id ELSE worker_id END
+      , success_count = success_count + num_successes
+      , error_count = error_count + CASE WHEN error_message IS NULL THEN 0 ELSE 1 END
+    WHERE vectorizer_id = worker_vectorizer_id;
+END;
+$$ LANGUAGE plpgsql security invoker
+set search_path to pg_catalog, pg_temp;

projects/extension/sql/incremental/019-add-worker-tracking-table.sql
@@ -1,0 +1,31 @@
+CREATE TABLE ai.vectorizer_worker_process(
+        id uuid not null primary key default gen_random_uuid()
+    ,   version text not null
+    ,   started timestamptz not null default now()
+    ,   expected_heartbeat_interval interval not null
+    ,   last_heartbeat timestamptz not null default now()
+    ,   heartbeat_count int not null default 0
+    ,   error_count int not null default 0
+    ,   success_count int not null default 0
+    ,   last_error_at timestamptz null default null
+    ,   last_error_message text null default null
+);
+
+create index on ai.vectorizer_worker_process (last_heartbeat);
+
+
+create table ai.vectorizer_worker_progress(
+      vectorizer_id int primary key not null references ai.vectorizer (id) on delete cascade
+    , success_count int not null default 0
+    , error_count int not null default 0
+    , last_success_at timestamptz null default null
+    -- don't use foreign key here because of three reasons:
+    -- 1. we don't want to enforce that the process exists in the process table (we may want to clean up that table independently)
+    -- 2. we don't want have any chance this row will fail to be inserted.
+    -- 3. we want the insert of this row to be as fast and lightweight as possible.
+    , last_success_process_id uuid null default null
+    , last_error_at timestamptz null default null
+    , last_error_message text null default null
+    --see reasons above for why we don't use foreign key here
+    , last_error_process_id uuid null default null
+);

projects/extension/tests/contents/output16.expected
@@ -100,15 +100,20 @@ CREATE EXTENSION
  function ai._vectorizer_vector_index_exists(name,name,jsonb)
  function ai.voyageai_embed(text,text,text,text,text,boolean)
  function ai.voyageai_embed(text,text[],text,text,text,boolean)
+ function ai._worker_heartbeat(uuid,integer,integer,text)
+ function ai._worker_progress(uuid,integer,integer,text)
+ function ai._worker_start(text,interval)
  sequence ai.vectorizer_id_seq
  table ai.feature_flag
  table ai.migration
  table ai._secret_permissions
  table ai.vectorizer
  table ai.vectorizer_errors
+ table ai.vectorizer_worker_process
+ table ai.vectorizer_worker_progress
  view ai.secret_permissions
  view ai.vectorizer_status
-(104 rows)
+(109 rows)
 
                                     Table "ai._secret_permissions"
  Column | Type | Collation | Nullable | Default | Storage  | Compression | Stats target | Description 
@@ -193,6 +198,7 @@ Indexes:
     "vectorizer_target_schema_target_table_key" UNIQUE CONSTRAINT, btree (target_schema, target_table)
 Referenced by:
     TABLE "ai.vectorizer_errors" CONSTRAINT "vectorizer_errors_id_fkey" FOREIGN KEY (id) REFERENCES ai.vectorizer(id) ON DELETE CASCADE
+    TABLE "ai.vectorizer_worker_progress" CONSTRAINT "vectorizer_worker_progress_vectorizer_id_fkey" FOREIGN KEY (vectorizer_id) REFERENCES ai.vectorizer(id) ON DELETE CASCADE
 Access method: heap
 
                                                 Table "ai.vectorizer_errors"
@@ -254,4 +260,57 @@ View definition:
  target_schema | cstring | yes  | target_schema | plain   | 
  target_table  | cstring | yes  | target_table  | plain   | 
 unique, btree, for table "ai.vectorizer"
+
+                                                          Table "ai.vectorizer_worker_process"
+           Column            |           Type           | Collation | Nullable |      Default      | Storage  | Compression | Stats target | Description 
+-----------------------------+--------------------------+-----------+----------+-------------------+----------+-------------+--------------+-------------
+ id                          | uuid                     |           | not null | gen_random_uuid() | plain    |             |              | 
+ version                     | text                     |           | not null |                   | extended |             |              | 
+ started                     | timestamp with time zone |           | not null | now()             | plain    |             |              | 
+ expected_heartbeat_interval | interval                 |           | not null |                   | plain    |             |              | 
+ last_heartbeat              | timestamp with time zone |           | not null | now()             | plain    |             |              | 
+ heartbeat_count             | integer                  |           | not null | 0                 | plain    |             |              | 
+ error_count                 | integer                  |           | not null | 0                 | plain    |             |              | 
+ success_count               | integer                  |           | not null | 0                 | plain    |             |              | 
+ last_error_at               | timestamp with time zone |           |          |                   | plain    |             |              | 
+ last_error_message          | text                     |           |          |                   | extended |             |              | 
+Indexes:
+    "vectorizer_worker_process_pkey" PRIMARY KEY, btree (id)
+    "vectorizer_worker_process_last_heartbeat_idx" btree (last_heartbeat)
+Access method: heap
+
+                  Index "ai.vectorizer_worker_process_last_heartbeat_idx"
+     Column     |           Type           | Key? |   Definition   | Storage | Stats target 
+----------------+--------------------------+------+----------------+---------+--------------
+ last_heartbeat | timestamp with time zone | yes  | last_heartbeat | plain   | 
+btree, for table "ai.vectorizer_worker_process"
+
+         Index "ai.vectorizer_worker_process_pkey"
+ Column | Type | Key? | Definition | Storage | Stats target 
+--------+------+------+------------+---------+--------------
+ id     | uuid | yes  | id         | plain   | 
+primary key, btree, for table "ai.vectorizer_worker_process"
+
+                                                   Table "ai.vectorizer_worker_progress"
+         Column          |           Type           | Collation | Nullable | Default | Storage  | Compression | Stats target | Description 
+-------------------------+--------------------------+-----------+----------+---------+----------+-------------+--------------+-------------
+ vectorizer_id           | integer                  |           | not null |         | plain    |             |              | 
+ success_count           | integer                  |           | not null | 0       | plain    |             |              | 
+ error_count             | integer                  |           | not null | 0       | plain    |             |              | 
+ last_success_at         | timestamp with time zone |           |          |         | plain    |             |              | 
+ last_success_process_id | uuid                     |           |          |         | plain    |             |              | 
+ last_error_at           | timestamp with time zone |           |          |         | plain    |             |              | 
+ last_error_message      | text                     |           |          |         | extended |             |              | 
+ last_error_process_id   | uuid                     |           |          |         | plain    |             |              | 
+Indexes:
+    "vectorizer_worker_progress_pkey" PRIMARY KEY, btree (vectorizer_id)
+Foreign-key constraints:
+    "vectorizer_worker_progress_vectorizer_id_fkey" FOREIGN KEY (vectorizer_id) REFERENCES ai.vectorizer(id) ON DELETE CASCADE
+Access method: heap
+
+               Index "ai.vectorizer_worker_progress_pkey"
+    Column     |  Type   | Key? |  Definition   | Storage | Stats target 
+---------------+---------+------+---------------+---------+--------------
+ vectorizer_id | integer | yes  | vectorizer_id | plain   | 
+primary key, btree, for table "ai.vectorizer_worker_progress"
 

projects/extension/tests/contents/output17.expected
@@ -100,12 +100,17 @@ CREATE EXTENSION
  function ai._vectorizer_vector_index_exists(name,name,jsonb)
  function ai.voyageai_embed(text,text,text,text,text,boolean)
  function ai.voyageai_embed(text,text[],text,text,text,boolean)
+ function ai._worker_heartbeat(uuid,integer,integer,text)
+ function ai._worker_progress(uuid,integer,integer,text)
+ function ai._worker_start(text,interval)
  sequence ai.vectorizer_id_seq
  table ai.feature_flag
  table ai.migration
  table ai._secret_permissions
  table ai.vectorizer
  table ai.vectorizer_errors
+ table ai.vectorizer_worker_process
+ table ai.vectorizer_worker_progress
  type ai.feature_flag
  type ai.feature_flag[]
  type ai.migration
@@ -120,9 +125,13 @@ CREATE EXTENSION
  type ai.vectorizer_errors[]
  type ai.vectorizer_status
  type ai.vectorizer_status[]
+ type ai.vectorizer_worker_process
+ type ai.vectorizer_worker_process[]
+ type ai.vectorizer_worker_progress
+ type ai.vectorizer_worker_progress[]
  view ai.secret_permissions
  view ai.vectorizer_status
-(118 rows)
+(127 rows)
 
                                     Table "ai._secret_permissions"
  Column | Type | Collation | Nullable | Default | Storage  | Compression | Stats target | Description 
@@ -207,6 +216,7 @@ Indexes:
     "vectorizer_target_schema_target_table_key" UNIQUE CONSTRAINT, btree (target_schema, target_table)
 Referenced by:
     TABLE "ai.vectorizer_errors" CONSTRAINT "vectorizer_errors_id_fkey" FOREIGN KEY (id) REFERENCES ai.vectorizer(id) ON DELETE CASCADE
+    TABLE "ai.vectorizer_worker_progress" CONSTRAINT "vectorizer_worker_progress_vectorizer_id_fkey" FOREIGN KEY (vectorizer_id) REFERENCES ai.vectorizer(id) ON DELETE CASCADE
 Access method: heap
 
                                                 Table "ai.vectorizer_errors"
@@ -268,4 +278,57 @@ View definition:
  target_schema | cstring | yes  | target_schema | plain   | 
  target_table  | cstring | yes  | target_table  | plain   | 
 unique, btree, for table "ai.vectorizer"
+
+                                                          Table "ai.vectorizer_worker_process"
+           Column            |           Type           | Collation | Nullable |      Default      | Storage  | Compression | Stats target | Description 
+-----------------------------+--------------------------+-----------+----------+-------------------+----------+-------------+--------------+-------------
+ id                          | uuid                     |           | not null | gen_random_uuid() | plain    |             |              | 
+ version                     | text                     |           | not null |                   | extended |             |              | 
+ started                     | timestamp with time zone |           | not null | now()             | plain    |             |              | 
+ expected_heartbeat_interval | interval                 |           | not null |                   | plain    |             |              | 
+ last_heartbeat              | timestamp with time zone |           | not null | now()             | plain    |             |              | 
+ heartbeat_count             | integer                  |           | not null | 0                 | plain    |             |              | 
+ error_count                 | integer                  |           | not null | 0                 | plain    |             |              | 
+ success_count               | integer                  |           | not null | 0                 | plain    |             |              | 
+ last_error_at               | timestamp with time zone |           |          |                   | plain    |             |              | 
+ last_error_message          | text                     |           |          |                   | extended |             |              | 
+Indexes:
+    "vectorizer_worker_process_pkey" PRIMARY KEY, btree (id)
+    "vectorizer_worker_process_last_heartbeat_idx" btree (last_heartbeat)
+Access method: heap
+
+                  Index "ai.vectorizer_worker_process_last_heartbeat_idx"
+     Column     |           Type           | Key? |   Definition   | Storage | Stats target 
+----------------+--------------------------+------+----------------+---------+--------------
+ last_heartbeat | timestamp with time zone | yes  | last_heartbeat | plain   | 
+btree, for table "ai.vectorizer_worker_process"
+
+         Index "ai.vectorizer_worker_process_pkey"
+ Column | Type | Key? | Definition | Storage | Stats target 
+--------+------+------+------------+---------+--------------
+ id     | uuid | yes  | id         | plain   | 
+primary key, btree, for table "ai.vectorizer_worker_process"
+
+                                                   Table "ai.vectorizer_worker_progress"
+         Column          |           Type           | Collation | Nullable | Default | Storage  | Compression | Stats target | Description 
+-------------------------+--------------------------+-----------+----------+---------+----------+-------------+--------------+-------------
+ vectorizer_id           | integer                  |           | not null |         | plain    |             |              | 
+ success_count           | integer                  |           | not null | 0       | plain    |             |              | 
+ error_count             | integer                  |           | not null | 0       | plain    |             |              | 
+ last_success_at         | timestamp with time zone |           |          |         | plain    |             |              | 
+ last_success_process_id | uuid                     |           |          |         | plain    |             |              | 
+ last_error_at           | timestamp with time zone |           |          |         | plain    |             |              | 
+ last_error_message      | text                     |           |          |         | extended |             |              | 
+ last_error_process_id   | uuid                     |           |          |         | plain    |             |              | 
+Indexes:
+    "vectorizer_worker_progress_pkey" PRIMARY KEY, btree (vectorizer_id)
+Foreign-key constraints:
+    "vectorizer_worker_progress_vectorizer_id_fkey" FOREIGN KEY (vectorizer_id) REFERENCES ai.vectorizer(id) ON DELETE CASCADE
+Access method: heap
+
+               Index "ai.vectorizer_worker_progress_pkey"
+    Column     |  Type   | Key? |  Definition   | Storage | Stats target 
+---------------+---------+------+---------------+---------+--------------
+ vectorizer_id | integer | yes  | vectorizer_id | plain   | 
+primary key, btree, for table "ai.vectorizer_worker_progress"
 

commit 3233f2e5d59913b240b86ccf736e96d2381da351

fix: do not exit when no extension installed
fixes a regression introduced in 3768c1c0a58e147fbeb6e720d7043f0fa6e0513a

projects/pgai/pgai/cli.py
@@ -339,14 +339,13 @@ async def async_run_vectorizer_worker(
                         await handle_error(
                             err_msg, None, worker_tracking, exit_on_error
                         )
-                        break
-                    features = Features(pgai_version)
-
-                    worker_tracking = WorkerTracking(
-                        db_url, poll_interval, features, __version__
-                    )
-                    await worker_tracking.start()
-                    can_connect = True
+                    else:
+                        features = Features(pgai_version)
+                        worker_tracking = WorkerTracking(
+                            db_url, poll_interval, features, __version__
+                        )
+                        await worker_tracking.start()
+                        can_connect = True
 
             if can_connect and features is not None and worker_tracking is not None:
                 if not dynamic_mode and len(valid_vectorizer_ids) != len(