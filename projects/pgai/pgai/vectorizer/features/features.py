cca97e68b0856b6d90d41f64faad36f968d8083b
Adol Rodriguez <adolsalamanca@gmail.com>
2025-03-21 17:07:01 +0100

    fix: port loading_retries to new features login

--- projects/pgai/pgai/vectorizer/features/features.py
+++ projects/pgai/pgai/vectorizer/features/features.py
@@ -1,13 +1,65 @@
 from functools import cached_property
+from typing import TypeVar
 
-from packaging import version
+import psycopg
+
+Self = TypeVar("Self", bound="Features")
 
 
 class Features:
     """Feature flags and version-dependent functionality manager."""
 
-    def __init__(self, ext_version: str) -> None:
-        self.ext_version = version.parse(ext_version)
+    def __init__(
+        self,
+            has_disabled_column: bool,
+            has_worker_tracking_table: bool,
+            has_loading_retries: bool,
+    ) -> None:
+        self.has_disabled_column = has_disabled_column
+        self.has_worker_tracking_table = has_worker_tracking_table
+        self.has_loading_retries = has_loading_retries
+
+    @classmethod
+    def from_db(cls: type[Self], cur: psycopg.Cursor) -> Self:
+        # for stuff that could be in extension or app,
+        # explicitly check for db features instead of versions
+        query = """
+        SELECT column_name
+        FROM information_schema.columns
+        WHERE table_schema = 'ai'
+              AND table_name = 'vectorizer'
+              AND column_name = 'disabled';
+        """
+        cur.execute(query)
+        has_disabled_column = cur.fetchone() is not None
+
+        query = """
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = 'ai' AND table_name = 'vectorizer_worker_process';
+        """
+        cur.execute(query)
+        has_worker_tracking_table = cur.fetchone() is not None
+
+        query = """
+        SELECT p.proname
+        FROM pg_proc p
+        JOIN pg_namespace n ON p.pronamespace = n.oid
+        WHERE p.proname = '_vectorizer_create_queue_failed_table'
+        AND n.nspname = 'ai'
+        """
+        cur.execute(query)
+        has_loading_retries = cur.fetchone() is not None
+
+        return cls(has_disabled_column, has_worker_tracking_table, has_loading_retries)
+
+    @classmethod
+    def for_testing_latest_version(cls: type[Self]) -> Self:
+        return cls(True, True, True)
+
+    @classmethod
+    def for_testing_no_features(cls: type[Self]) -> Self:
+        return cls(False, False, False)
 
     @cached_property
     def disable_vectorizers(self) -> bool:
@@ -16,12 +68,12 @@ class Features:
         The feature consists of a `disabled` column in the `ai.vectorizer`
         table, and the `ai.vectorizer_status` view.
         """
-        return self.ext_version > version.parse("0.7.0")
+        return self.has_disabled_column
 
     @cached_property
     def worker_tracking(self) -> bool:
         """If the worker tracking feature is supported by the extension."""
-        return self.ext_version > version.parse("0.8.0")
+        return self.has_worker_tracking_table
 
     @cached_property
     def loading_retries(self) -> bool:
@@ -30,4 +82,4 @@ class Features:
         The feature includes changes in the way we fetch_work from the
         queueing tables, and also how we handle the retries.
         """
-        return self.ext_version > version.parse("0.9.0")
+        return self.has_loading_retries
