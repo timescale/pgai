408e6b2391511169849cadc65200dc63b814e938
Matthew Peveler <mpeveler@timescale.com>
2025-04-18 10:27:04 -0600

    chore: install timescale into public schema

--- projects/pgai/tests/semantic_catalog/data/cagg.sql
+++ projects/pgai/tests/semantic_catalog/data/cagg.sql
@@ -1,5 +1,5 @@
-create extension if not exists timescaledb with schema postgres_air cascade;
-create extension if not exists timescaledb_toolkit with schema postgres_air cascade;
+create extension if not exists timescaledb with schema public cascade;
+create extension if not exists timescaledb_toolkit with schema public cascade;
 
 create table if not exists postgres_air.events
 (
@@ -7,12 +7,12 @@ create table if not exists postgres_air.events
     name text not null,
     params jsonb not null
 );
-SELECT postgres_air.create_hypertable('postgres_air.events', postgres_air.by_range('time'));
+SELECT public.create_hypertable('postgres_air.events', public.by_range('time'));
 
 create materialized view postgres_air.events_daily
 with (timescaledb.continuous) as
 select
     name,
-    postgres_air.time_bucket(INTERVAL '1 day', time) as bucket
+    public.time_bucket(INTERVAL '1 day', time) as bucket
 from postgres_air.events
 group by name, bucket;
