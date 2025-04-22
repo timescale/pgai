a56361486950b633233f11975ad91a0af5e5507d
Matvey Arye <cevian@gmail.com>
2025-04-22 15:20:36 -0400

    fix: turn off ddtrace by default (#639)

--- projects/pgai/pgai/__init__.py
+++ projects/pgai/pgai/__init__.py
@@ -2,4 +2,8 @@ __version__ = "0.10.0-dev"
 
 from pgai._install.install import ainstall, install
 
+from .tracing import configure_tracing
+
+configure_tracing()
+
 __all__ = ["ainstall", "install"]
---
+++ projects/pgai/pgai/tracing.py
@@ -0,0 +1,11 @@
+import os
+
+from ddtrace import tracer
+
+
+def configure_tracing():
+    # Disable tracer by default
+    if os.getenv("DD_TRACE_ENABLED", "false").lower() == "true":
+        tracer.enabled = True
+    else:
+        tracer.enabled = False
