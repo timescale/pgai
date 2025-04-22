539abc8fa4772c050f6ef52de55e7c527494e862
Matvey Arye <cevian@gmail.com>
2025-04-22 16:10:03 -0400

    chore: fix stray

--- projects/pgai/pgai/vectorizer/__init__.py
+++ projects/pgai/pgai/vectorizer/__init__.py
@@ -1,9 +1,6 @@
 from .create_vectorizer import CreateVectorizer
 from .vectorizer import Executor, Vectorizer
 from .worker import Worker
-from ddtrace import tracer
-
-tracer.enabled = False
 
 __all__ = [
     "Vectorizer",
