d584d70c70199fd4b8c6d8fcbfe899a67c5cf058
John Pruitt <jgpruitt@gmail.com>
2025-04-23 12:03:16 -0500

    chore: add logging to semantic-catalog

--- projects/pgai/pgai/semantic_catalog/vectorizer/ollama.py
+++ projects/pgai/pgai/semantic_catalog/vectorizer/ollama.py
@@ -2,12 +2,17 @@ from collections.abc import Sequence
 
 from ollama import AsyncClient
 
-from pgai.semantic_catalog.vectorizer import EmbedRow, OllamaConfig
+from pgai.semantic_catalog.vectorizer import OllamaConfig
+from pgai.semantic_catalog.vectorizer.models import EmbedRow
 
 
 async def embed_batch(config: OllamaConfig, batch: list[EmbedRow]) -> None:
     client = AsyncClient(host=config.base_url)  # TODO: cache this?
     response = await client.embed(config.model, [x.content for x in batch])
+    if len(response.embeddings) != len(batch):
+        raise RuntimeError(
+            f"{len(batch)} items sent to ollama but {len(response.embeddings)} embeddings returned"  # noqa
+        )
     for i, embed in enumerate(response.embeddings):
         batch[i].vector = embed
 
@@ -15,4 +20,6 @@ async def embed_batch(config: OllamaConfig, batch: list[EmbedRow]) -> None:
 async def embed_query(config: OllamaConfig, query: str) -> Sequence[float]:
     client = AsyncClient(host=config.base_url)  # TODO: cache this?
     response = await client.embed(config.model, query)
+    if len(response.embeddings) != 1:
+        raise RuntimeError(f"expected 1 embedding but got {len(response.embeddings)}")
     return response.embeddings[0]
