5e32c229caed9c47f26f62d6f24e778bf209834b
Sergio Moya <1083296+smoya@users.noreply.github.com>
2025-05-08 14:09:50 +0200

    feat: rename index instead

--- projects/pgai/db/sql/incremental/030-add_vectorizer_errors_view.sql
+++ projects/pgai/db/sql/incremental/030-add_vectorizer_errors_view.sql
@@ -1,26 +1,9 @@
 -- rename the ai.vectorizer_errors table to ai._vectorizer_errors
 alter table ai.vectorizer_errors rename to _vectorizer_errors;
 
--- drop any existing indexes on the old table that would have been associated with the (id, recorded) columns
--- this is for index naming consistency purpose, not strictly necessary
-do $$
-declare
-  _index_name text;
-begin
-  select indexname into _index_name
-  from pg_indexes
-  where schemaname = 'ai' 
-    and tablename = 'vectorizer_errors'
-    and indexdef like '%id, recorded%';
-    
-  if _index_name is not null then
-    execute 'drop index if exists ai.' || quote_ident(_index_name);
-  end if;
-end
-$$;
-
--- recreate the previous index to perform lookups by vectorizer id
-create index on ai._vectorizer_errors (id, recorded);
+-- rename the existing index on the ai.vectorizer_error so it follows the right naming convention (adds the _ prefix)
+-- this is not strictly necessary, but it is a good practice to keep the naming consistent
+alter index ai.vectorizer_errors_id_recorded_idx rename to _vectorizer_errors_id_recorded_idx;
 
 -- create a view including vectorizer name
 create or replace view ai.vectorizer_errors as
@@ -32,7 +15,7 @@ from
   left join ai.vectorizer v on ve.id = v.id;
 
 
--- grant privileges on _vectorizer_errors and vectorizer_errors to vectorizer users
+-- grant privileges on vectorizer_errors
 DO $$
 DECLARE
     to_user text;
